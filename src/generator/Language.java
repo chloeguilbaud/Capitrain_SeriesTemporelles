6c296a816f52e10c8d1391c438d0a56fb1c9de78
Chloe <44chloeguilbaud@gmail.com>
2018-10-29 21:43:23 +0100

    feat: Generator init implementation

--- src/generator/Language.java
+++ src/generator/Language.java
@@ -1,4 +1,11 @@
 package generator;
 
+import model.decoration.table.DecorationTable;
+import model.seed.transducer.SeedTransducer;
+
 public interface Language {
+
+    StringBuffer generateCode(SeedTransducer seedTransducer,
+                        DecorationTable decorationTable);
+
 }
--- src/generator/java/JavaClass.java
+++ src/generator/java/JavaClass.java
@@ -1,6 +1,14 @@
 package generator.java;
 
 import generator.Language;
+import model.decoration.table.DecorationTable;
+import model.seed.transducer.SeedTransducer;
 
 public class JavaClass implements Language {
+
+    @Override
+    public StringBuffer generateCode(SeedTransducer seedTransducer, DecorationTable decorationTable) {
+        return null;
+    }
+
 }
