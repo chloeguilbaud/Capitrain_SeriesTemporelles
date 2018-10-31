9549d502f930bb06bcaa05e53da223ae24f9e9db
Chloe <44chloeguilbaud@gmail.com>
2018-10-31 15:28:52 +0100

    feat: Seed Transducer model

--- src/model/seed/transducer/SeedTransducer.java
+++ src/model/seed/transducer/SeedTransducer.java
@@ -1,17 +1,20 @@
 package model.seed.transducer;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class SeedTransducer {
 
     private String name;
-    private HashSet<State> states;
-    private HashSet<Link> links;
+    private State initState;
+    private HashMap<String, State> states;
+    private HashMap<String, Link> links;
     private int nodeDistance;
     private String startStateKey;
 
-    public SeedTransducer(String name, HashSet<State> states, HashSet<Link> links, int nodeDistance, String startStateKey) {
+    public SeedTransducer(String name, State initState, HashMap<String, State> states, HashMap<String, Link> links, int nodeDistance, String startStateKey) {
         this.name = name;
+        this.initState = initState;
         this.states = states;
         this.links = links;
         this.nodeDistance = nodeDistance;
---
+++ src/parser/seed/transducer/SeedTransducerJSONElements.java
@@ -0,0 +1,37 @@
+package parser.seed.transducer;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum SeedTransducerJSONElements {
+
+    SEED_TEMPLATE("seed_template"),
+    SEED_TEMPLATE_NAME("name"),
+    SEED_TEMPLATE_INIT_STATE("init_state"),
+    SEED_TEMPLATE_STATES("states"),
+    SEED_TEMPLATE_ARCS("arcs"),
+
+    SEED_TEMPLATE_ARC_FROM("from"),
+    SEED_TEMPLATE_ARC_TO("to"),
+    SEED_TEMPLATE_OPERATOR("operator"),
+    SEED_TEMPLATE_LETTER("letter");
+
+    private String label;
+
+    SeedTransducerJSONElements(String lab) {
+        this.label = lab;
+    }
+
+    public static SeedTransducerJSONElements fromLabel(String lab) {
+        return valuesAsList().stream().filter(m -> m.getLabel().equalsIgnoreCase(lab)).findAny().orElse(null);
+    }
+
+    public static List<SeedTransducerJSONElements> valuesAsList() {
+        return Arrays.asList(values());
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+}
