9f473692c53661f0d3ec437c3e61da255a1c8a9b
Chloe <44chloeguilbaud@gmail.com>
2018-11-18 17:18:12 +0100

    docs: Decoraton table parser and entry point

--- src/main/java/parser/decoration/table/DecorationTableParser.java
+++ src/main/java/parser/decoration/table/DecorationTableParser.java
@@ -2,10 +2,14 @@ package parser.decoration.table;
 
 import parser.decoration.table.process.DecorationTableConverter;
 import parser.decoration.table.process.DecorationTableParsingResult;
+import model.decoration.table.DecorationTable;
 
 import java.io.File;
-import java.io.IOException;
 
+/**
+ * Utils class and entry point to the {@link DecorationTable} parser extracting data from the given JSON file.
+ * @author Chloé GUILBAUD & Maël MAINCHAIN
+ */
 class DecorationTableParser {
 
     static DecorationTableParsingResult parse(File jsonFile) {
