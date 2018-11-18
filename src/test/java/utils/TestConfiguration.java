cd797f62e2d5ce0c9c3ecaa637fcc9f01770188f
Chloe <44chloeguilbaud@gmail.com>
2018-11-18 19:07:55 +0100

    refactor: Parser example JSON files

--- src/test/java/utils/TestConfiguration.java
+++ src/test/java/utils/TestConfiguration.java
@@ -2,7 +2,7 @@ package utils;
 
 public enum TestConfiguration {
 
-    TEST_FILE_PATH("src/test/resources/"),
+    TEST_FILE_PATH("src/test/resources/parser/examples/"),
     TEST_FILE_PATH_SEED_TRANSDUCER_PARSER("src/test/resources/parser/seed/transducer/"),
     TEST_FILE_PATH_DECORATION_TABLE_PARSER("src/test/resources/parser/decoration/table/");
 
