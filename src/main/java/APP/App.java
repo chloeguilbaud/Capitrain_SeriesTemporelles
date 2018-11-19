0ffd8bdb241bf440083f13c8e46811c1377cf68e
Chloe <44chloeguilbaud@gmail.com>
2018-11-19 20:59:36 +0100

    feat: App main class

---
+++ src/main/java/APP/App.java
@@ -0,0 +1,25 @@
+package APP;
+
+import generator.GeneratorManager;
+
+/**
+ * Soft application.
+ * @author Chloé GUILBAUD & Maël MAINCHAIN
+ */
+public class App {
+
+    public static void main (String[] args) {
+
+        Class<String> cc = String.class;
+
+        // TODO: get transducer and table from file
+        // SeedTransducer seedTransducer = initTransducer();
+        // DecorationTable decorationTable = initDecorationTable();
+
+        // StringBuffer javaCode = generator.generateCode(Generator.JAVA, seedTransducer, decorationTable);
+
+        // System.out.println(javaCode.toString());
+
+    }
+
+}
