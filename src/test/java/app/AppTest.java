459f435c77246330ae1765ddeb2552d1aa599adb
Chloe <44chloeguilbaud@gmail.com>
2018-11-21 11:27:53 +0100

    test: Main app

---
+++ src/test/java/app/AppTest.java
@@ -0,0 +1,31 @@
+package app;
+
+import org.junit.Test;
+import utils.TestConfiguration;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    private final String path = TestConfiguration.TEST_FILE_PATH_APP.getValue();
+
+    @Test
+    public void appTest_GenerationWithWritingInFile() {
+
+        App.main(new String[] {
+                path + "SeedTransducerPeak.json",
+                path + "DecorationTableFeature.json",
+                "Java"});
+
+    }
+
+    @Test
+    public void appTest_GenerationWithoutWritingInFile() {
+        App.main(new String[] {
+                path + "SeedTransducerPeak.json",
+                path + "DecorationTableFeature.json",
+                "Java",
+                path});
+    }
+
+}
